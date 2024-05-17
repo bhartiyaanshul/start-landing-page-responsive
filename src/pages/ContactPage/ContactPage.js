6d10d54f8b363d5d80a7aee1902cabb090018a98
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-05-17 14:07:25 +0530

    google maps integrated

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -18,7 +18,7 @@ export const ContactPage = () => {
                 <textarea className='form-input'></textarea>
                 <button className='submit-button'>Submit</button>
             </form>
-            <img src='images/map.png' alt=''/>
+            <iframe  className='maps' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3759.9312860500063!2d72.59181765286958!3d23.103963681145487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e83b16dbefd51%3A0x3ae59a1059dd8ab1!2sCodeDecoders!5e0!3m2!1sen!2sin!4v1715934714767!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </section>
     </div>
   )
